5aecc41f35490825a9c2a78f46a604b9f2245b80
KIMCHOONGMAN <cm730712@gmail.com>
2024-05-01 02:00:47 +0900

    [Modify] 밋업 디테일 페이지 반응형 구현

--- src/components/MeetUpDetail/MeetUpDetail.jsx
+++ src/components/MeetUpDetail/MeetUpDetail.jsx
@@ -27,39 +27,40 @@ export default function MeetUpDetail() {
   } = useLoaderData();
 
   return (
-    <div className="pb-10 pt-50pxr">
+    <div className="pb-10 pt-50pxr @container">
       {/* 태그를 proprs로 전달하는 법은?? */}
       {/* <MeetUpItem info={data} /> */}
       <DetailHeader title="자세히" isBottomSheet={undefined} />
       <MeetupCarousel />
-      <div className="mx-20pxr mb-20pxr min-h-120pxr min-w-320pxr rounded-xl bg-white px-20pxr py-15pxr shadow">
-        <h3 className="mb-4pxr text-base font-extrabold leading-snug text-primary">
-          {eventTitle}
-        </h3>
-        <h4 className="mb-10pxr text-sm font-extrabold leading-tight text-gray-500">
-          {cafeName}
-        </h4>
+      <div className="mt-6 grid grid-cols-1 gap-2 @desktop:grid-cols-2 @desktop:grid-rows-2">
+        {/*첫번째 */}
+        <div className="mx-20pxr mb-20pxr min-h-120pxr min-w-320pxr rounded-xl bg-white px-20pxr py-15pxr shadow">
+          <h3 className="mb-4pxr text-base font-extrabold leading-snug text-primary">
+            {eventTitle}
+          </h3>
+          <h4 className="mb-10pxr text-sm font-extrabold leading-tight text-gray-500">
+            {cafeName}
+          </h4>
 
-        <MeetUpDetailItem title="주소" content={address} />
-        <MeetUpDetailItem title="트위터" content={source} />
-        <MeetUpDetailItem title="영업기간" content={date} />
-      </div>
-
-      <div className="mx-20pxr flex flex-col rounded-xl bg-white shadow">
-        <MeetUpDetailItemContainer
-          title="GIFT"
-          content={
-            <>
-              <MeetUpDetailItem title="기본특전" content={basicGift} />
-              <MeetUpDetailItem title="선착특전" content={priorityGift} />
-              <MeetUpDetailItem title="쿠키특전" content={desertGift} />
-            </>
-          }
-        />
-        <MeetUpDetailItemContainer
-          title="EVENT"
-          content={
-            <>
+          <MeetUpDetailItem title="주소" content={address} />
+          <MeetUpDetailItem title="트위터" content={source} />
+          <MeetUpDetailItem title="영업기간" content={date} />
+        </div>
+        {/*두ㅡ번째 */}
+        <div className="mx-20pxr flex flex-col rounded-xl bg-white shadow">
+          <MeetUpDetailItemContainer
+            title="GIFT"
+            content={
+              <>
+                <MeetUpDetailItem title="기본특전" content={basicGift} />
+                <MeetUpDetailItem title="선착특전" content={priorityGift} />
+                <MeetUpDetailItem title="쿠키특전" content={desertGift} />
+              </>
+            }
+          />
+          <MeetUpDetailItemContainer
+            title="EVENT"
+            content={
               <MeetUpDetailItem
                 title="해쉬태그"
                 content={
@@ -69,14 +70,18 @@ export default function MeetUpDetail() {
                   </ul>
                 }
               />
-            </>
-          }
-        />
+            }
+          />
+        </div>
+        {/*지도 */}
+        <div className="mx-20pxr mt-6 flex h-300pxr flex-col rounded-xl shadow">
+          <MeetUpDetailMap lat={lat} lng={lng} cafeName={cafeName} />
+        </div>
+        {/*댓글 */}
+        <div className="">
+          <CommentContainer id={id} commentList={comments} />
+        </div>
       </div>
-      <div className="mx-20pxr mt-6 flex h-300pxr flex-col rounded-xl shadow">
-        <MeetUpDetailMap lat={lat} lng={lng} cafeName={cafeName} />
-      </div>
-      <CommentContainer id={id} commentList={comments} />
     </div>
   );
 }
