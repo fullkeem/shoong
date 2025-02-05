88c0a4f7eccd8ba3235d70d3e4f78f50232979d0
Choongman Kim <cm730712@gmail.com>
2025-02-06 03:25:40 +0900

    Refact: 작성자 데이터를 하나씩 가져오는 방식 => 필터 문자열을 통해 한번에 가져오는 방식

--- src/api/userDataApi.js
+++ src/api/userDataApi.js
@@ -1,19 +1,27 @@
+// /api/userDataApi.js
 import pb from './pocketbase';
 
 /**
- * 작성자 데이터를 가져오는 함수
- * @param {Array<string>} writerIds - 작성자 ID 배열
- * @returns {Promise<Array>} - 작성자 데이터 배열
+ * 작성자 데이터들을 가져오는 함수
+ * @param {Array<string>} writerIds - 작성자 ID 배열 (예: ["rsj6pb36wux36w5","rnrex8l58qj58rh"])
  */
-
 export const fetchUsersData = async (writerIds) => {
   try {
     pb.autoCancellation(false);
-    const uniqueWriterIds = [...new Set(writerIds)]; // 중복 제거
-    const users = await Promise.all(
-      uniqueWriterIds.map((id) => pb.collection('users').getOne(id))
-    );
-    return users;
+    const uniqueWriterIds = [...new Set(writerIds)];
+    if (uniqueWriterIds.length === 0) return [];
+
+    // 예: id = "rsj6pb36wux36w5" || id = "rnrex8l58qj58rh"
+    const filterString = uniqueWriterIds
+      .map((id) => `id = "${id}"`)
+      .join(' || ');
+
+    // 한 번에 유저 목록 불러오기
+    const userList = await pb.collection('users').getFullList({
+      filter: filterString,
+    });
+
+    return userList;
   } catch (error) {
     console.error('Error fetching users data:', error);
     throw error;
