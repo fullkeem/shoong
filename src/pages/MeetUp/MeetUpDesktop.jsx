6dd9d86de52c7b6f02636673640ef5a3aa9dda36
Choongman Kim <cm730712@gmail.com>
2025-02-17 00:12:32 +0900

    Refact: 밋업 페이지 카드 클릭 시 해당 marker focust 및 지도 중심 이동

--- src/pages/MeetUp/MeetUpDesktop.jsx
+++ src/pages/MeetUp/MeetUpDesktop.jsx
@@ -7,7 +7,7 @@ export default function MeetUpDesktop({ meetUpData }) {
     <div className="flex h-full w-full flex-row">
       <div className="relative flex h-full w-1/4 flex-row justify-center border-t-gray-50">
         <SearchBar
-          name={'mapSearch'}
+          name="mapSearch"
           placeholder={'장소,아티스트 이름'}
           bgStyle={'absolute top-2 mx-1 py-3 w-11/12 bg-white'}
         />
